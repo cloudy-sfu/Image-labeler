90485555cafed89624bd29bcf4decaf379348dc8
cloudy-sfu <41314224+cloudy-sfu@users.noreply.github.com>
2026-03-26 11:58:43 +1300

    Fix starting port

--- app.py
+++ app.py
@@ -476,6 +476,6 @@ def find_available_port(start_port: int, tries: int = 100):
 
 
 if __name__ == '__main__':
-    port = find_available_port(5000)
+    port = find_available_port(1024)
     webbrowser.open_new_tab(f'http://localhost:{port}')
     app.run(port=port)
